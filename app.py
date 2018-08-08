f5eba0d9a53e0a8c1ea9e1cd5b7af6cb61a26fa7
Satish mankar <satishmankar745@gmail.com>
2018-08-08 19:07:57 +0530

    Add By Satish

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,request
+from textblob import TextBlob
+from wtforms import Form,StringField,TextAreaField,validators
+
+
+
+app = Flask(__name__)
+
+class homepageForm(Form):
+    sentiments = TextAreaField('Text', render_kw={"rows": 5, "cols": 11})
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    formx = homepageForm(request.form)
+    senti = ''
+    if request.method == 'POST':
+        sen = formx.sentiments.data
+        x = TextBlob(sen)
+        pol = x.sentiment.polarity
+        if pol < 0 :
+            senti = 'Given sentence have negative polarity'
+        else:
+            senti = 'Given sentence have positive polarity'
+        return render_template('home.html',form=formx,sent=senti,pol=pol)
+    else:
+        return render_template('home.html',form=formx,sent=senti,pol=pol)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
